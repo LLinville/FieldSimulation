c02fa97be2a3c0b061bcea633f5f4f09ea5487c2
Eracoy <thebluefire39@gmail.com>
2019-10-26 17:34:12 -0400

    second order update equation with x,y vel. No phase vel

--- 2d_complex.py
+++ 2d_complex.py
@@ -34,12 +34,15 @@ def np_fftconvolve(A):
     return np.real(ifft2(fft2(A)*fft2([[1/9]*3]*3, s=A.shape)))
 
 def conv(field):
-    return signal.convolve2d(field,  [[1/9]*3]*3, mode='same')
+    return signal.convolve2d(field, np.array([[1/25]*5]*5), mode='same', boundary='fill', fillvalue=0)
 
-def add_point(field, location, size=10, polarity=1):
-    field[location[0] - size // 2 : location[0] + size // 2, location[1] - size // 2 : location[1] + size // 2] = np.array(
-        [[polarity / size * u + 1j / size * v for u in range(-1 * size // 2, size // 2)] for v in range(-1*size//2, size//2)]
+def add_point(field, location, size=10, polarity=1, rotation = 0):
+    point = np.array(
+        [[
+            (polarity / size * u + 1j * polarity / size * v)
+            for u in range(-1 * size // 2, size // 2)] for v in range(-1*size//2, size//2)]
     )
+    field[location[0] - size // 2 : location[0] + size // 2, location[1] - size // 2 : location[1] + size // 2] = point * (np.cos(rotation) + np.sin(rotation) * 1j)
 
 SIZE = 100
 x, y = np.meshgrid(np.linspace(0, 1, SIZE), np.linspace(0, 1, SIZE))
@@ -53,11 +56,26 @@ field = np.zeros_like(z)
 plt.show()
 
 # add_point(field, (30, 30), 10)
-add_point(field, (40, 40), 10, -10)
-add_point(field, (60, 60), 10, 10)
+polarity = 10
+for x in [40, 60, 80]:
+    for y in [40, 60, 80]:
+        # add_point(field, (x,y), 6, 1*polarity)
+        polarity *= 1
 
+for i in range(1,8)[::-1]:
+    add_point(field, (60, 60), i*2, 0.1 ** (i))
+# add_point(field, (60, 60), 10, 1)
+# add_point(field, (60, 60), 10, 1, pi/2)
+
+
+vel = np.zeros_like(field)
 for i in range(1000000):
-    field = conv(field) * 1.01
+    print(i)
+    vel -= field * 0.01
+    prev_field = field.copy()
+    alpha = 1
+    field = (1 - alpha) * field + alpha * conv(field) + vel * 1
+
     img = colorize(field)
     plt.imshow(img)
     plt.draw()
